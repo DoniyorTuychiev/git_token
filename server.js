c71d0e93d4f8710670077e8e48413753825c5f91
doni <doni0101195@gmail.com>
2023-09-30 01:40:02 +0900

    MIT: MonGoDB ga Ulanish

--- server.js
+++ server.js
@@ -1,41 +1,35 @@
 
 
-console.log("web Serverni boshlash");
-const express = require("express");
-const app = express(); // bu yerda expressdan app nomli instens yasaldi
 const http = require("http");
-//1 Kirish Code
-//bu bosqichda Expressga kirib kelyotgan malumotlarga bogliq codelar yoziladi. 
-app.use(express.static("public"));// bu harbir brauzerdan kirip kelyotkan sorovlar uchun public yani ochiq degani
-app.use(express.json()); //express.json(); kirip kelyotkan json formatdagi malumotlarni jsondan object formatga ozgartirib berishini bildiradi
-app.use(express.urlencoded ({extended: true})); // bu urlencoded html dagi form dan post qilingan narsalarni qabulqiladi agar bu yozilmasa ignore qiladi
+const mongodb = require("mongodb");
 
-//2 Session Code
+let db;
+const connectionString = //bu orinda MongoDB ni ulab olamiz 
+"mongodb+srv://doni:kjil35Fo8aHsyx80@cluster0.xbvoiwi.mongodb.net/Reja";
 
-//3 Views Code
+mongodb.connect(// mongo db ichida connec nomli mehod bor va u 3 a paramer oladi 1) Sring 2)
+    connectionString, //1-Sring
+    {
+        useNewUrlParser: true,//2- true bilan useNewUrlParser va true bilan useUnifiedTopology
+        useUnifiedTopology: true, 
+    }, 
+    (err, client) => {// 3- parametr bu callBack boladi
+        try{ //2 - qadam
+            console.log("MongoDB connection succead");
+            
+            module.exports = client; //server.js ni app.js ga ulash uchun module.export qildim
+                                    //client ichida db nomli object bor shuning uchun export qildik va app ichida chaqiriladi 
+            
+            const app = require("./views/app");//client mongoDB ga ulangach app.js ga boglanadi va portga uzatadi
+            const server = http.createServer(app);
+            let PORT = 4000;
+            server.listen(PORT, function () {
+                console.log(`The server is running successfully on part : ${PORT}, http://localhost:${PORT}`//http://localhost:${PORT} orqali linkniyam consolda kora olamiz
+                );
+            });   
 
-app.set("views", "views"); //htmlni backendda qurilishi uchun view yasaladi va view engine "ejs " ekani korsatiladi.
-app.set("view engine", "ejs"); // views folder ochiladi agar folder name ozgarsa < app.set("views", "views"); >bu yerda ham ozgartirish kerak boladi
-
-//4 Routing Code keyingibosqichda rooterlar uchun
-
-app.post("/create-item", (req, res) => { //post malumotni olib kelib dataBase ga yozadi. Postda req ni tarkibida body qismida malumot keladi; 
-    //Request : 3 qisimdan iborat.
-    // 1- URL qismi(starting line). yuqorida u "/create-item" ga tog'ri keladi
-    // 2- qismi: bosh qismi(HTTP Rquest Headers) 
-    // 3 - qismi: bu body qismi  
-
-    console.log(req.body); // kelgan narsani body qismini chiqarish
-    res.json({test:"success"}); // res.json => bu json shaklda malumotni qaytarish funcini bajaradi ; req => require, res => response
-});
-
-app.get('/', function(req, res){ // get malumotni oqish uchun ishlatiladi
-    res.render("reja");//harid nomli frameWorkkiga bog'lanadi va frontend qismini ishka tushurib beradi
-});
-
-const server = http.createServer(app);
-let PORT = 4000;
-server.listen(PORT, function () {
-    console.log(`The server is running successfully on part : ${PORT}, http://localhost:${PORT}`
-    );
-});
+        }catch{ //1 - qadam
+                // bu functionda catch Erorni tekshiriladi agar eror yoq bolsa yani => MongoDB ga client muvofaqyatli ulansa 2- qadam ishlaydi
+            console.log("ERROR on connection MongoDB");
+        }  
+    });
---
+++ views/app.js
@@ -0,0 +1,38 @@
+
+
+console.log("web Serverni boshlash");
+const express = require("express");
+const app = express(); // bu yerda expressdan app nomli instens yasaldi
+const http = require("http");
+
+//Mongo DB ulash
+//Mongo DB ni ulash 2 xil usulda amlga oshiriladi
+
+// 1- usul ogridan ogri ulash
+//2 - usul Mongus Packege orqali ishlaish
+
+//db nomli ozgaruvchi qabul qilib uni server.js dan malumo oldi
+
+const db = require("../server").db(); // bu yerda / oldidan faqat bitta . (nuqta) qiyilsa bazan eror(not found './server') bolishi mumkin
+                                //shuning uchun / oldidan ..(2)ta qoyish orqali erorni bartaraf qilamiz
+
+app.use(express.static("public"));
+app.use(express.json()); 
+app.use(express.urlencoded ({extended: true})); 
+
+app.set("views", "views"); 
+app.set("view engine", "ejs"); 
+
+//4 Routing Code keyingibosqichda rooterlar uchun
+
+app.post("/create-item", (req, res) => { //post malumotni olib kelib dataBase ga yozadi. Postda req ni tarkibida body qismida malumot keladi; 
+    
+    console.log(req.body); 
+    res.json({test:"success"}); 
+});
+
+app.get('/', function(req, res){ 
+    res.render("reja");
+});
+
+module.exports = app;
